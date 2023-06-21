f7c9dd14d067eb9744064a19431ba8709c1adfd7
Ludlin1999 <lin.yuchuan0519@gmail.com>
2023-06-21 10:29:52 -0400

    the problem that name is not saved on mongodb is solved

--- controllers/products.js
+++ controllers/products.js
@@ -1,24 +1,12 @@
-const Product = require("../models/product");
-
 function newProduct(req, res) {
   res.render("products/new.ejs");
 }
 
-// function create(req, res) {
-//   req.body.published = !!req.body.published;
-//   const product = new Product(req.body);
-//   product.save(function (err) {
-//     if (err) return res.render("products/new.ejs");
-//     console.log(product);
-//     res.redirect("/products/new");
-//   });
-// }
-
 async function create(req, res) {
   try {
+    const Product = require("../models/product");
     req.body.published = !!req.body.published;
-    const product = new Product(req.body);
-
+    const product = req.body;
     const doc = await Product.create(product);
     console.log(doc);
   } catch (err) {
@@ -30,3 +18,54 @@ module.exports = {
   new: newProduct,
   create: create,
 };
+
+// const Product = require("../models/product").model;
+
+// function newProduct(req, res) {
+//   res.render("products/add");
+// }
+
+// async function index(req, res) {
+//   try {
+//     const products = await Product.find({});
+//     res.render("products/index", { title: "Products", products });
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
+
+// async function create(req, res) {
+//   try {
+//     const product = new Product(req.body);
+//     const savedProduct = await product.save();
+//     console.log(savedProduct);
+//     res.redirect("/products");
+//   } catch (err) {
+//     console.error(err);
+//     res.redirect("/products/add");
+//   }
+// }
+
+// async function show(req, res) {
+//   try {
+//     const product = await Product.findById(req.params.id);
+//     res.render("products", { title: "All Products", product });
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
+
+// async function deleteProduct(req, res) {
+//   try {
+//     const product = await Product.findByIdAndRemove(req.params.id);
+//     if (!product) {
+//       return res.status(404).send();
+//     }
+//     res.redirect("/products");
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send(err);
+//   }
+// }
+
+// module.exports = { index, create, newProduct, show, deleteProduct };
